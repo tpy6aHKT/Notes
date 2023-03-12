8aa2bb832dbb7b307169f489c2300f154eac0640
Vadim Smolin <master@Vadims-Mac-mini.local>
2023-03-12 15:26:02 +0500

    before MVVM

---
+++ ToDoShka/Views/AboutPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoShka.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
